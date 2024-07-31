d55fbc679c4aa292f2034799d149304f4dfcc05a
Marcelo <mmarcmartins@gmail.com>
2024-07-31 17:31:28 -0300

    Fix select auto complete behavior

--- src/components/SelectAutoComplete/index.tsx
+++ src/components/SelectAutoComplete/index.tsx
@@ -2,6 +2,7 @@
 import type { ChangeEvent, KeyboardEvent } from "react";
 import { memo, useEffect, useRef, useState } from "react";
 import { useThrottle } from "../../hooks/useThrottle";
+import './index.scss';
 
 type SelectAutoCompleteProps = {
   items: string[];
@@ -17,9 +18,13 @@ export const SelectAutoComplete = memo(({ items, onSelectItem, placeholder}: Sel
   const inputRef = useRef<HTMLInputElement | null>(null);
   const selectedRef = useRef<HTMLUListElement | null>(null);
 
-  const scrollIntoItem = () => {
+  useEffect(() => {
+    onSelectItem(filter[activeIndex]);
+  }, [activeIndex]);
+
+  const scrollIntoItem = () => {    
     const selected = selectedRef.current?.querySelector(".selected-item");
-    selected?.scrollIntoView({ behavior: "smooth", block: "start" });
+    selected?.scrollIntoView({ behavior: "smooth", block: "nearest" });
   };
 
   useEffect(() => {
@@ -34,32 +39,32 @@ export const SelectAutoComplete = memo(({ items, onSelectItem, placeholder}: Sel
         setUserInput(filter[activeIndex]);
         setIsOpened(false);
         setFilter([]);
-        setActiveIndex(0);
+        setActiveIndex(0);        
         onSelectItem(filter[activeIndex]);
         break;
       case "ArrowUp":        
         setActiveIndex((prev) => {
-            onSelectItem(filter[activeIndex]);
-            return prev > 0 ? prev - 1 : prev          
-          }
-        );        
+        const newIdx = prev > 0 ? prev - 1 : prev
+        onSelectItem(filter[newIdx]);
+          return newIdx;
+        });        
         setTimeout(scrollIntoItem, 100);
         break;
       case "ArrowDown":
         setActiveIndex((prev) => {
-            onSelectItem(filter[activeIndex]);
-            return prev < filter.length - 1 ? prev + 1 : prev;         
-          }
-        );           
+          const newIdx = prev < filter.length - 1 ? prev + 1 : prev;
+          onSelectItem(filter[newIdx]);
+          return newIdx
+        });           
         setTimeout(scrollIntoItem, 100);
         break;
     }
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newInput = e.target.value.toLowerCase();
+    const newInput = e.target.value;
     const filtered = items.filter((item) =>
-      item.toLowerCase().includes(newInput)
+      item.toLowerCase().includes(newInput.toLocaleLowerCase())
     );
     setUserInput(newInput);
     setFilter(filtered);
@@ -69,7 +74,7 @@ export const SelectAutoComplete = memo(({ items, onSelectItem, placeholder}: Sel
 
   const handleItemClick = (item: string) => {
     setUserInput(item);
-    onSelectItem(item);
+    
     setIsOpened(false);
     setFilter([]);
     setActiveIndex(0);
@@ -106,13 +111,10 @@ export const SelectAutoComplete = memo(({ items, onSelectItem, placeholder}: Sel
           {filter.map((curr, index) => (
             <li
               key={curr}
-              className={index === activeIndex ? "selected-item" : "default-item"}
-              dangerouslySetInnerHTML={{
-                __html: curr.toLowerCase().replace(userInput, `<span class="match">${userInput}</span>`)
-              }}
+              className={index === activeIndex ? "selected-item" : "default-item"}              
               role="option"
               onClick={() => handleItemClick(curr)}
-            />
+            >{curr}</li>
           ))}
         </ul>
       )}
