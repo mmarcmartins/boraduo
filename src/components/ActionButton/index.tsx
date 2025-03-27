b8d0294d0e58b1e5f7313d19fcb4900c207f23e0
marcelo martins <ext.marcelo.martins@gympass.com>
2025-03-27 09:39:34 -0300

    fix 404 link button, add link to header and create link button component

--- src/components/ActionButton/index.tsx
+++ src/components/ActionButton/index.tsx
@@ -2,26 +2,24 @@ import { ClickSpark } from "../ClickSpark";
 import "./styles.scss";
 import type { ButtonHTMLAttributes } from "react";
 
-type ActionButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & React.PropsWithChildren;
+type ActionButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  React.PropsWithChildren;
 
-export const ActionButton = ({    
+export const ActionButton = ({
   className = "",
   children,
   ...buttonProps
 }: ActionButtonProps) => {
   return (
-    <button       
-      className={`action-button ${className}`}
-      {...buttonProps}
-    >      
+    <button className={`action-button ${className}`} {...buttonProps}>
       <ClickSpark
-        sparkColor='#fff'
+        sparkColor="#fff"
         sparkSize={10}
         sparkRadius={15}
         sparkCount={8}
         duration={400}
       >
-      {children}
+        {children}
       </ClickSpark>
     </button>
   );
